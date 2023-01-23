0714393aa800dd8146addfda74b3ff9895a0b69d
Maxim <danilov.neru@yandex.ru>
2023-01-23 22:49:33 +0900

    решение задачи и описание решения в файлы README.md

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+var mass = new string[] { "name", "age", "day", "no", "numbers"};
+
+string[] group = Array.FindAll(mass, elem => elem.Length <= 3);
+foreach (var elem in group) Console.WriteLine(elem);
